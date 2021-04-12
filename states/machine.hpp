9b2ac81ad60d8a2c69b9a7fc6731c9dc0e63150a
Daniel Pav <danny_pav@hotmail.com>
2021-04-11 21:05:05 -0400

    commenting

--- states/machine.hpp
+++ states/machine.hpp
@@ -17,22 +17,21 @@ template<typename... TLinks>
 class Machine
 {
 public:
-    /* */
+    /* list of unique states that are start or end states */
     typedef TypeListUnique<typename TLinks::TFromType..., typename TLinks::TToType...> TStateTypes;
-    /* */
+    /* typenum representing a state from the list of unique states */
     typedef TypeNum<TStateTypes> TStateNum;
-    /* */
+    /* list of unique events */
     typedef TypeListUnique<typename TLinks::TEventType...> TEventTypes;
-    /* */
+    /* typenum representing an event from the list of unique events */
     typedef TypeNum<TEventTypes> TEventNum;
-    /* */
-    
+
 private:
     /* */
     typedef TypeList<typename TLinks::TKeyType...> TKeyTypes;
     /* */
     typedef TypeListUnique<typename TLinks::TKeyType...> TUniqueKeyTypes;
-    /* */
+    /* asserts that the list of links does not have any duplicate start, event pairs */
     static_assert(TypeListSize<TKeyTypes>::size == TypeListSize<TUniqueKeyTypes>::size,
                   "set of links must have unique set of from/event pairs.");
 
@@ -104,14 +103,17 @@ public:
         return handleImpl<TEvent, TData, TLinks...>(state, data);
     }
 
-    /* */
+    /* handles the transition from the state using the event given
+     advances the state, returns true if tthe state is valid and
+     can be advanced */
     template<typename TData>
     static bool handle(TStateNum& state, const TEventNum& event, TData& data)
     {
         return handleImpl<TData, TLinks...>(state, event, data);
     }
 
-    /* process the current given state */
+    /* process the current given state, without advancing in any way
+        returns true if state is valid */
     template<typename TData>
     static bool process(const TStateNum& state, TData& data)
     {
